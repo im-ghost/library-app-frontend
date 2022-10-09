c9c56057eb8cab24f938dd2f46754a41fb0c9761
Adetonwa Richard <culestfrosh@gmail.com>
2022-10-09 22:06:36 +0100

    Homepage

---
+++ pages/index.tsx
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import {
+  Typography,
+  Button,
+  Container ,
+  Card
+} from "@mui/material"
+
+
+export default function Home() {
+
+  return ( <div>
+   <Head>
+    <title> Library </title> 
+    <meta name = "description" content = "Library app home page" /> 
+    <meta property="og:type" content="website"/>
+    <meta property="og:image" content="/books.jpg"/>
+    <meta property="og: description" content="Library app home page"/>
+    <link rel = "icon" href = "/favicon.ico" />
+   </Head> 
+   <Card className="homepage">
+     <img src="/books.jpg" className="homepage__image"/>
+     <Typography variant="h1" className="homepage__text">
+     We grow by learning everyday
+     </Typography>
+     <Button className="homepage__button">
+     <Link href="/books">
+     <Typography variant="h4" className="homepage__button--text">
+     Read a Book today 
+     </Typography>
+     </Link>
+     </Button>
+     </Card>
+  </div>
+  )
+}
